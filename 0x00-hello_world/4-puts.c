69f3d9e66b75be1ec0b29bb00ebdb17b4f5438e5
yassine elbahi <yassine.jr200@gmail.com>
2023-12-14 23:16:52 +0000

    f

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,15 @@
-#!/bin/bash
-puts ("programming is like building a multilingual puzzle\n")
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * description: a C program that prints with put function
+ *
+ * Return: always 0 (Succes)
+*/
+
+int main(void)
+{
+	puts("\"programming is like building a multilingual puzzle\n");
+	return (0);
+}
