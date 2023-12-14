5702b69493cd1fa316655d0f891296262d9beaa0
yassine elbahi <yassine.jr200@gmail.com>
2023-12-14 23:24:48 +0000

    d

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * description: a C program that prints with printf function
+ *
+ * Return: always 0 (Succes)
+*/
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
+
