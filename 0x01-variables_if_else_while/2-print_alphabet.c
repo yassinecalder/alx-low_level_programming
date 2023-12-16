da163573f9a1814587a26aed1b4a0a58934448f9
yassine elbahi <yassine.jr200@gmail.com>
2023-12-16 01:46:11 +0000

    d

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * description: a C program that prints with putchar function
+ *
+ * Return: always 0 (Succes)
+*/
+
+int main(void)
+{
+	int alpha = 97;
+
+	while (alpha <= 122)
+	{
+		putchar (alpha);
+		alpha++;
+	}
+	return (0);
+}
