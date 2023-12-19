a79678dc3567024ee7c1b8a56692861362e59391
yassine elbahi <yassine.jr200@gmail.com>
2023-12-19 00:55:51 +0000

    d

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ *main - entry point
+ *
+ * desctiption: print base16 numbers
+ *
+ * Return: always 0 (succes)
+*/
+
+int main(void)
+{
+	int a = 48;
+
+	while (a <= 102)
+	{
+		putchar(a);
+		if (a == 57)
+			a += 57;
+		a++;
+	}
+	putchar('\n');
+
+	return (0);
+}
